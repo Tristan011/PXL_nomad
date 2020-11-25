acb6cef8c8ab51bc0122a7258b1de9ed163397ed
Tristan011 <tristan.keena@student.pxl.be>
2020-11-25 18:45:43 +0100

    Ansible werkt eindelijk

--- scripts/configclient.sh
+++ scripts/configclient.sh
@@ -6,7 +6,7 @@ sudo sed -i '/10.0.4.67/c\retry_join=[\"192.168.1.4\"]' /etc/consul.d/consul.hcl
 
 ## Nomad
 sudo sed -i '/bind_addr/c\bind_addr = "{{ GetInterfaceIP \\"eth1\\" }}"' /etc/nomad.d/nomad.hcl
-sudo sed -i '/client {/a\ \ network_interface = "eth1"' /etc/nomad.d/nomad.hcl
+#sudo sed -i '/client {/a\ \ network_interface = "eth1"' /etc/nomad.d/nomad.hcl
 sudo sed -i '/servers/c\  servers = ["192.168.1.4:4646"]' /etc/nomad.d/nomad.hcl
 #sed -i '7s/true/false/' nomad.hcl
 
